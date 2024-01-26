2147d9b1213734fe97267c5aadad00ed888f4f55
jren03 <juntaoren44@gmail.com>
2024-01-26 14:39:33 -0500

    wip

--- MujocoSysID/mbrl/util/psdp_fetch_demos.py
+++ MujocoSysID/mbrl/util/psdp_fetch_demos.py
@@ -14,35 +14,25 @@ else:
     data_root = Path("/share/portal/jlr429/pessimistic-irl/expert_data")
 
 
-def fetch_demos(env_name, cfg):
+def fetch_demos(env_name, cfg=None):
     env_name = env_name.replace("gym___", "")
 
-    if cfg.psdp_wrapper:
-        possible_data_path = Path(data_root, f"{env_name}_psdp_demos.npz")
-    else:
-        possible_data_path = Path(data_root, f"{env_name}_demos.npz")
-
+    possible_data_path = Path(data_root, f"{env_name}_psdp_demos.npz")
     if possible_data_path.exists():
         print(f"Loading from {possible_data_path}")
         data = np.load(possible_data_path, allow_pickle=True)
-        print(f"{np.mean(data['rewards'])=}")
-        # dataset = {
-        #     "observations": data["observations"],
-        #     "actions": data["actions"],
-        #     "next_observations": data["next_observations"],
-        #     "rewards": data["rewards"],
-        #     "terminals": data["terminals"],
-        # }
-        # if cfg.train_discriminator:
-        #     dataset["rewards"] = np.zeros_like(data["rewards"])
+        dataset = {
+            "observations": data["observations"],
+            "actions": data["actions"],
+            "next_observations": data["next_observations"],
+            "rewards": data["rewards"],
+            "terminals": data["terminals"],
+        }
+        if cfg.train_discriminator:
+            dataset["rewards"] = np.zeros_like(data["rewards"])
+        print(f"{np.mean(dataset['rewards'])=}")
         return (
-            {
-                "observations": data["observations"],
-                "actions": data["actions"],
-                "next_observations": data["next_observations"],
-                "rewards": data["rewards"],
-                "terminals": data["terminals"],
-            },
+            dataset,
             torch.from_numpy(data["expert_sa_pairs"]),
             data["qpos"],
             data["qvel"],
@@ -53,7 +43,6 @@ def fetch_demos(env_name, cfg):
     if "maze" in env_name:
         env = gym.make(env_name)
         dataset = env.get_dataset()
-
         term = np.argwhere(
             np.logical_or(dataset["timeouts"] > 0, dataset["terminals"] > 0)
         )
@@ -62,8 +51,16 @@ def fetch_demos(env_name, cfg):
         for i in range(len(term)):
             expert_ranges.append([start, term[i][0] + 1])
             start = term[i][0] + 1
-        q_dataset = d4rl.qlearning_dataset(env)
+        expert_ranges = np.array(expert_ranges)
+        obs_ref = {}
+        for exp_range in expert_ranges:
+            for i, obs in enumerate(
+                dataset["observations"][exp_range[0] : exp_range[1]]
+            ):
+                # add timestep
+                obs_ref[tuple(obs)] = i
 
+        q_dataset = d4rl.qlearning_dataset(env)
         curr_obs_pt = 0
         curr_obs_indices = []
         next_obs_pt = 0
@@ -101,11 +98,32 @@ def fetch_demos(env_name, cfg):
         terminals = q_dataset["terminals"]
         actions = q_dataset["actions"]
 
+        # add timestep to observations in q_dataset and dataset
+        new_observations = np.zeros((observations.shape[0], observations.shape[1] + 1))
+        new_next_observations = np.zeros(
+            (next_observations.shape[0], next_observations.shape[1] + 1)
+        )
+        for i, (obs_g, next_obs_g) in enumerate(zip(observations, next_observations)):
+            new_observations[i] = np.concatenate(
+                (
+                    observations[i],
+                    np.array([obs_ref[tuple(obs_g[: -len(goals_flat[0])])]]),
+                ),
+                axis=0,
+            )
+            new_next_observations[i] = np.concatenate(
+                (
+                    next_observations[i],
+                    np.array([obs_ref[tuple(next_obs_g[: -len(goals_flat[0])])]]),
+                ),
+                axis=0,
+            )
+
         new_dataset = {
-            "observations": observations,
+            "observations": new_observations,
             "actions": actions,
-            "next_observations": next_observations,
-            "rewards": rewards,
+            "next_observations": new_next_observations,
+            "rewards": np.zeros_like(rewards),
             "terminals": terminals,
         }
 
@@ -118,9 +136,18 @@ def fetch_demos(env_name, cfg):
             goals.append(goals_flat[start : term[i][0] + 1])
             start = term[i][0] + 1
 
-        obs_goal_cat = np.concatenate(
-            [dataset["observations"], dataset["infos/goal"]], axis=1
+        obs_goal_cat = np.zeros(
+            (
+                dataset["observations"].shape[0],
+                dataset["observations"].shape[1] + dataset["infos/goal"].shape[1] + 1,
+            )
         )
+        for obs, goal in zip(dataset["observations"], dataset["infos/goal"]):
+            obs_goal_cat[i] = np.concatenate(
+                (obs, goal, np.array([obs_ref.get(tuple(obs), 0)])),
+                axis=0,
+            )
+
         expert_reset_states = np.array(
             [
                 obs_goal_cat[expert_ranges[i][0] : expert_ranges[i][1]]
