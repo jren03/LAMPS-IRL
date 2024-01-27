494fe5e4d46a6f1b6cca46cc9da5c57402b3857e
jren03 <juntaoren44@gmail.com>
2024-01-27 14:06:53 -0500

    wip

--- MujocoSysID/mbrl/examples/main.py
+++ MujocoSysID/mbrl/examples/main.py
@@ -11,13 +11,6 @@ from termcolor import cprint
 
 import mbrl.algorithms.lamps_am as lamps_am
 import mbrl.algorithms.model_pretraining as mp
-import mbrl.algorithms.lamps_am_mb as lamps_am_mb
-import mbrl.algorithms.lamps_am_mf as lamps_am_mf
-import mbrl.algorithms.lamps_am_hie as lamps_am_hie
-import mbrl.algorithms.lamps_am_psdp as lamps_am_psdp
-# import mbrl.algorithms.lamps_am_sac_buf as lamps_am_sac_buf
-# import mbrl.algorithms.mm as mm
-
 import mbrl.util.env
 
 
@@ -49,14 +42,6 @@ def run(cfg: omegaconf.DictConfig):
 
     if cfg.algorithm.name == "lamps_am":
         return lamps_am.train(env, test_env, term_fn, cfg, silent=cfg.silent)
-    # elif cfg.algorithm.name == "lamps_am_mb":
-    #     return lamps_am_mb.train(env, test_env, term_fn, cfg, silent=cfg.silent)
-    # if cfg.algorithm.name == "lamps_am_psdp":
-    #     return lamps_am_psdp.train(env, test_env, term_fn, cfg, silent=cfg.silent)
-    # elif cfg.algorithm.name == "lamps_am_mf":
-    #     return lamps_am_mf.train(env, test_env, term_fn, cfg, silent=cfg.silent)
-    # elif cfg.algorithm.name == "lamps_am_hie":
-    #     return lamps_am_hie.train(env, test_env, term_fn, cfg, silent=cfg.silent)
     elif cfg.algorithm.name == "model_pretraining":
         return mp.train(env, cfg)
     else:
